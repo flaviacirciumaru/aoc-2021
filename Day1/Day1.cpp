5f9d69f9da35f78a3e3e82beda9252c9e8a4f5cd
flaviacirciumaru <flavia@github.com>
2021-12-02 23:20:58 +0000

    Part 2 with a couple of rectifications for day 1

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -8,15 +8,18 @@ using namespace std;
 
 std::list<int> ParseDepths(string fileName);
 int GetNumberOfIncreasesFromList(std::list<int> depths);
+std::list<int> GetThreeMeasurementsSums(std::list<int> depths);
 
 int main() {
     std::list<int> depths;
     string depth;
     int increases = 0;
 
-    depths = ParseDepths("input.txt");
+     depths = ParseDepths("input.txt");
+     //depths = {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+    auto threeMeasurementsSums = GetThreeMeasurementsSums(depths);
 
-    std::cout << "Increases: " << GetNumberOfIncreasesFromList(depths) << endl;
+    std::cout << "Increases: " << GetNumberOfIncreasesFromList(threeMeasurementsSums) << endl;
 
     return 1;
 }
@@ -29,24 +32,39 @@ std::list<int> ParseDepths(string fileName) {
     while ( getline(ReadFile, depth) ) {
         int intDepth = std::stoi(depth);
         
-        depths.push_front(intDepth);
+        depths.push_back(intDepth);
     }
     return depths;
 }
 
 int GetNumberOfIncreasesFromList(std::list<int> depths) {
 
-    std::vector<int> depthsInt(depths.begin(), depths.end());
+    std::vector<int> depthsVector(depths.begin(), depths.end());
     int increases = 0;
 
-    for (auto i = 0; i < depthsInt.size() - 1; i++) {
+    for (auto i = 0; i < depthsVector.size() - 1; i++) {
 
-        if (depthsInt[i] > depthsInt[i + 1]) {
+        if (depthsVector[i] < depthsVector[i + 1]) {
 
-            std::cout << depthsInt[i] << " > " << depthsInt[i + 1] << endl;
+            std::cout << depthsVector[i] << " < " << depthsVector[i + 1] << endl;
             increases ++;
         }
     }
 
     return increases;
+}
+
+std::list<int> GetThreeMeasurementsSums (std::list<int> depths) {
+    std::list<int> threeMeasurementsSums;
+    std::vector<int> depthsVector(depths.begin(), depths.end());
+
+    for (auto i = 0; i < depthsVector.size() - 2; i++) {
+        auto sum  = depthsVector[i] + depthsVector[i + 1] + depthsVector[i + 2];
+        std::cout << sum << " = " << depthsVector[i] << " + " << depthsVector[i + 1] << " + " << depthsVector[i + 2] << endl;
+        threeMeasurementsSums.push_back(sum);
+    }
+
+    std::cout << "length  " << threeMeasurementsSums.size() << endl;
+
+    return threeMeasurementsSums;
 }
