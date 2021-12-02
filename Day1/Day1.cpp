9c73669984833360cf37c12f8c1f84c9e0123456
flaviacirciumaru <flavia@github.com>
2021-12-02 22:34:57 +0000

    Refactor into functions

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -6,22 +6,38 @@
 
 using namespace std;
 
+std::list<int> ParseDepths(string fileName);
+int GetNumberOfIncreasesFromList(std::list<int> depths);
+
 int main() {
     std::list<int> depths;
     string depth;
-    int increases = 0; // having fun remembering to declare variables, otherwise shit happens
+    int increases = 0;
 
-    ifstream ReadFile("input.txt");
+    depths = ParseDepths("input.txt");
+
+    std::cout << "Increases: " << GetNumberOfIncreasesFromList(depths) << endl;
+
+    return 1;
+}
+
+std::list<int> ParseDepths(string fileName) {
+    ifstream ReadFile(fileName);
+    std::list<int> depths;
+    string depth;
 
     while ( getline(ReadFile, depth) ) {
         int intDepth = std::stoi(depth);
         
         depths.push_front(intDepth);
-
-        std::cout << "Added depth: " << intDepth << endl;
     }
+    return depths;
+}
+
+int GetNumberOfIncreasesFromList(std::list<int> depths) {
 
     std::vector<int> depthsInt(depths.begin(), depths.end());
+    int increases = 0;
 
     for (auto i = 0; i < depthsInt.size() - 1; i++) {
 
@@ -29,11 +45,8 @@ int main() {
 
             std::cout << depthsInt[i] << " > " << depthsInt[i + 1] << endl;
             increases ++;
-
         }
     }
 
-    std::cout << "Increases: " << increases << endl;
-
-    return 1;
+    return increases;
 }
